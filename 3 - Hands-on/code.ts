343906ecc1ce2f02c40d1f7479aa8dc81638fa4f
Bia Almeida <beatriz.06.06.2016@gmail.com>
2026-04-18 17:07:12 -0300

    :sparkles: Add logic to register people

--- 3 - Hands-on/code.ts
+++ 3 - Hands-on/code.ts
@@ -3,10 +3,32 @@ import * as promptSync from "prompt-sync";
 
 const prompt = promptSync()
 
+// build a model
+type Person = {
+    name: string
+    age: number
+}
+
+// list
+const people: Person[] = []
+
 let action: number = 1
 
 while (action != 5) {
-    action = prompt("Type the action you want: ")
-    console.log(action)
+    let input: string = prompt("Type the action you want: ")
+    action = Number(input)
     
+    if (action === 1) {
+        console.log("\nOk, let's register a new person!")
+
+        let name: string = prompt("Type the person's name: ")
+
+        let age_input: string = prompt("Type the person's age: ")
+        let age: number = Number(age_input)
+
+        people.push({name, age})
+        console.log()
+    }
 }
+
+console.log(people)
